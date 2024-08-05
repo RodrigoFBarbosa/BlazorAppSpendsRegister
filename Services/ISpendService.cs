369043770d8a471a9adc853dcc83a26edf950133
RodrigoFBarbosa <rodrigobarbosa.p99@gmail.com>
2024-08-05 17:25:29 -0300

    Adicionar arquivos de projeto.

---
+++ Services/ISpendService.cs
@@ -0,0 +1,9 @@
+﻿using BlazorAppSpendsRegister.Models;
+
+namespace BlazorAppSpendsRegister.Services;
+
+public interface ISpendService
+{
+    Task<List<Spend>> GetSpends();
+    Task SaveSpends(List<Spend> spends);
+}
---
+++ Services/SpendService.cs
@@ -0,0 +1,34 @@
+﻿using BlazorAppSpendsRegister.Models;
+using Blazored.LocalStorage;
+using System.Text.Json;
+
+namespace BlazorAppSpendsRegister.Services
+{
+    public class SpendService : ISpendService
+    {
+        private readonly ILocalStorageService _localStorageService;
+        public SpendService(ILocalStorageService localStorageService)
+        {
+            _localStorageService = localStorageService;
+        }
+
+        private string spendsLocalStorageKey => "spendKey";
+
+        public async Task<List<Spend>> GetSpends()
+        {
+            var spendsJson = await _localStorageService.GetItemAsync<string>(spendsLocalStorageKey);
+            if (string.IsNullOrEmpty(spendsJson))
+                return new();
+
+            return JsonSerializer.Deserialize<List<Spend>>(spendsJson);
+
+        }
+
+        public async Task SaveSpends(List<Spend> spends)
+        {
+            var spendsJson = JsonSerializer.Serialize(spends);
+
+            await _localStorageService.SetItemAsync(spendsLocalStorageKey, spendsJson);
+        }
+    }
+}
